7b4817980485d04bd6ba9cf333ebc28cb4898d59
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-09 18:02:08 +0000

    Refactor forEach to reducer function

--- 2/1.js
+++ 2/1.js
@@ -1,7 +1,5 @@
 let fs = require("fs");
 
-const passwordsWithRules = fs.readFileSync("input").toString().split("\n");
-
 const destructurePassword = (passwordWithRule) => {
   const raw = passwordWithRule.split(" ");
   return [raw[0].split("-").map((n) => parseInt(n, 10)), raw[1][0], raw[2]];
@@ -13,18 +11,14 @@ const count = (char, string) => {
     .reduce((count, c) => (c === char ? count + 1 : count), 0);
 };
 
-let validPasswordCount = 0;
+const passwordsWithRules = fs.readFileSync("input").toString().split("\n");
 
-passwordsWithRules.forEach((passwordWithRule) => {
-  const [ruleRange, ruleLetter, password] = destructurePassword(
-    passwordWithRule
-  );
-
-  const charCount = count(ruleLetter, password);
-
-  if (charCount >= ruleRange[0] && charCount <= ruleRange[1]) {
-    validPasswordCount += 1;
-  }
-});
+const validPasswordCount = passwordsWithRules.reduce((acc, cur) => {
+  const [ruleRange, ruleLetter, password] = destructurePassword(cur);
+  return count(ruleLetter, password) >= ruleRange[0] &&
+    count(ruleLetter, password) <= ruleRange[1]
+    ? (acc += 1)
+    : acc;
+}, 0);
 
 console.log(validPasswordCount);
