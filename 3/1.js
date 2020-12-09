06ffdb30dc15469d9304d8ac39a23c469973e89d
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-09 20:09:22 +0000

    Refactor out magic constant

--- 3/1.js
+++ 3/1.js
@@ -1,12 +1,13 @@
 let fs = require("fs");
 
 const rows = fs.readFileSync("input").toString().split("\n");
+const row_length = rows[0].length;
+const horizontal_movement = 3;
 
-let currentIndex = 0;
+let x_position = 0;
 const treeCount = rows.reduce((acc, cur) => {
-  let cell = cur.repeat(300)[currentIndex];
-  currentIndex += 3;
-
+  let cell = cur.repeat(row_length * horizontal_movement)[x_position];
+  x_position += 3;
   return cell === "#" ? (acc += 1) : acc;
 }, 0);
 
