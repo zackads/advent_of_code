409ea3eefc9e9e6cd5bcfca9007bd0863b960b17
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-09 18:11:37 +0000

    Day 2 part 2

---
+++ 2/2.js
@@ -0,0 +1,32 @@
+let fs = require("fs");
+
+const destructurePassword = (passwordWithRule) => {
+  const raw = passwordWithRule.split(" ");
+  return [raw[0].split("-").map((n) => parseInt(n, 10)), raw[1][0], raw[2]];
+};
+
+const count = (char, string) => {
+  return string
+    .split("")
+    .reduce((count, c) => (c === char ? count + 1 : count), 0);
+};
+
+const valid = (char, positions, string) => {
+  // Is char at exactly one of positions in string?
+  return (
+    positions.reduce((acc, cur) => {
+      const zeroIndexedPosition = cur - 1;
+      return (acc += string[zeroIndexedPosition] === char ? 1 : 0);
+    }, 0) === 1
+  );
+};
+
+const passwordsWithRules = fs.readFileSync("input").toString().split("\n");
+
+const validPasswordCount = passwordsWithRules.reduce((acc, cur) => {
+  const [ruleRange, ruleLetter, password] = destructurePassword(cur);
+
+  return valid(ruleLetter, ruleRange, password) ? (acc += 1) : acc;
+}, 0);
+
+console.log(validPasswordCount);
