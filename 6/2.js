4aca5d9c482531cc89c38dca0a366e440ac61315
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-11 13:25:09 +0000

    Delete unused helper functions

--- 6/2.js
+++ 6/2.js
@@ -9,24 +9,13 @@ const countGroup = (group) => {
   return common(...people).length;
 };
 
-const homogenous = (arr) => {
-  return Object.keys(histogram(arr)).length <= 1;
-};
-
 const histogram = (arr) => {
   return arr.reduce((acc, cur) => ({ ...acc, [cur]: (acc[cur] || 0) + 1 }), {});
-};
-
-const duplicates = (histogramObj) => {
-  return Object.keys(histogramObj).filter((value) => histogramObj[value] > 1);
 };
 
 const uniques = (histogramObj) => {
   return Object.keys(histogramObj).filter((value) => histogramObj[value] >= 1);
 };
-
-const commonProperties = (o1, o2) =>
-  Object.keys(o1).filter((item) => item in o2);
 
 const common = (...strings) => {
   let all_chars = uniques(histogram(strings.join("").split("")));
@@ -43,9 +32,6 @@ console.log(group_answers.reduce((acc, cur) => acc + countGroup(cur), 0));
 
 module.exports = {
   histogram: histogram,
-  duplicates: duplicates,
-  homogenous: homogenous,
   countGroup: countGroup,
-  commonProperties: commonProperties,
   common: common,
 };
--- 6/2.test.js
+++ 6/2.test.js
@@ -1,12 +1,5 @@
 const { expect, describe } = require("@jest/globals");
-const {
-  countGroup,
-  duplicates,
-  histogram,
-  homogenous,
-  commonProperties,
-  common,
-} = require("./2");
+const { countGroup, histogram, common } = require("./2");
 
 describe("histogram", () => {
   const expectations = [
@@ -20,65 +13,6 @@ describe("histogram", () => {
       test_case.output
     )}`, () => {
       expect(histogram(test_case.input)).toEqual(test_case.output);
-    });
-  });
-});
-
-describe("duplicates", () => {
-  const expectations = [
-    { input: { a: 1, b: 1, c: 1 }, output: [] },
-    { input: { a: 2, c: 1 }, output: ["a"] },
-    { input: { c: 3 }, output: ["c"] },
-  ];
-
-  expectations.forEach((test_case) => {
-    test(`input ${JSON.stringify(test_case.input)} returns [${
-      test_case.output
-    }] `, () => {
-      expect(duplicates(test_case.input)).toEqual(test_case.output);
-    });
-  });
-});
-
-describe("homogenous", () => {
-  const expectations = [
-    { input: [], output: true },
-    { input: ["a", "a"], output: true },
-    { input: ["ab", "ab"], output: true },
-  ];
-
-  expectations.forEach((test_case) => {
-    test(`input ${JSON.stringify(test_case.input)} returns [${
-      test_case.output
-    }] `, () => {
-      expect(homogenous(test_case.input)).toEqual(test_case.output);
-    });
-  });
-});
-
-describe("commonProperties", () => {
-  const expectations = [
-    {
-      input: [
-        { a: 1, b: 1 },
-        { a: 2, b: 3 },
-      ],
-      output: ["a", "b"],
-    },
-    {
-      input: [
-        { a: 1, b: 1 },
-        { a: 2, c: 3 },
-      ],
-      output: ["a"],
-    },
-  ];
-
-  expectations.forEach((test_case) => {
-    test(`input ${JSON.stringify(test_case.input)} returns [${
-      test_case.output
-    }] `, () => {
-      expect(commonProperties(...test_case.input)).toEqual(test_case.output);
     });
   });
 });
