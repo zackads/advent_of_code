fef9fde5e90d368aaa0274fdd46cef3889bee2d3
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-13 12:05:38 +0000

    Refactor to reduce function

--- 7/1.js
+++ 7/1.js
@@ -11,97 +11,31 @@ const parseRule = (rule) => {
     .split(" bags contain ")[1]
     .split(", ")
     .reduce((bags, cur) => {
-      return cur.includes("no other bags") ? [] : [...bags, parseBag(cur)];
+      return cur.includes("no other bags")
+        ? []
+        : [...bags, `${cur.split(" ")[1]} ${cur.split(" ")[2]}`];
     }, []);
   return { [outer_bag]: inner_bags };
 };
 
-const parseBag = (bag) => `${bag.split(" ")[1]} ${bag.split(" ")[2]}`;
-
-class Queue {
-  constructor() {
-    this.elements = [];
-  }
-
-  enqueue(...elements) {
-    elements.forEach((e) => this.elements.push(e));
-  }
-
-  dequeue() {
-    return this.elements.shift();
-  }
-
-  isEmpty() {
-    return this.elements.length === 0;
-  }
-
-  peek() {
-    return !this.isEmpty() ? this.elements[0] : undefined;
-  }
-
-  length() {
-    return this.elements.length;
-  }
-}
-
-/*
-Be sure to parse the input into some data structure where you can easily 
-query the contents of a bag by its name. It's not necessary, but recursion
-might be the best way to start to unwrap this one; any bag _contains 
-the shiny gold one_ if 1) the shiny gold bag is directly in 
-its contents, or 2) one of the bags in its contents _contains the shiny 
-gold one_.
-*/
-
-const graph = {};
-rules.forEach((rule) => Object.assign(graph, parseRule(rule)));
-
-let checked = [];
-const containsShinyGold = (bag) => {
-  let counter = 0;
-  console.log(checked);
-  if (!checked.includes(graph[bag])) {
-    if (graph[bag].includes("shiny gold")) {
-      counter += 1;
-      checked.push(bag);
-    } else {
-      counter += graph[bag].reduce(
-        (count, bag) => (count += containsShinyGold(bag)),
-        0
-      );
-    }
-  }
-  return counter;
+const makeGraph = (rules) => {
+  const graph = {};
+  rules.forEach((rule) => Object.assign(graph, parseRule(rule)));
+  return graph;
 };
 
 const countShinyGoldBags = (graph) => {
   const outer_bags = Object.keys(graph);
-  counter = 0;
-  outer_bags.forEach((bag) => {
-    if (
-      graph[bag].includes("shiny gold") ||
-      includesShinyGoldBag(graph, graph[bag])
-    ) {
-      counter += 1;
-    }
-  });
-  return counter;
+  return outer_bags.reduce((count, bag) => {
+    return includesShinyGoldBag(bag, graph) ? (count += 1) : count;
+  }, 0);
 };
 
-const includesShinyGoldBag = (graph, outer_bags) => {
-  let contains = false;
-  outer_bags.forEach((bag) => {
-    if (
-      graph[bag].includes("shiny gold") ||
-      includesShinyGoldBag(graph, graph[bag])
-    ) {
-      contains = true;
-    }
-  });
-  return contains;
-};
+const includesShinyGoldBag = (bag, graph) =>
+  graph[bag].includes("shiny gold") ||
+  graph[bag].some((bag) => includesShinyGoldBag(bag, graph));
 
-console.log(countShinyGoldBags(graph));
+console.log(countShinyGoldBags(makeGraph(rules)));
 
 module.exports = {
   parseRule: parseRule,
