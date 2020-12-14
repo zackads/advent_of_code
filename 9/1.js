6ca6ddbbd154e2ff8bc952a8b977750a83514e34
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-14 10:11:17 +0000

    Day 9 part 1

--- 9/1.js
+++ 9/1.js
@@ -1,6 +1,31 @@
-const firstNumNotSumOfTwoPreceedingNNumbers = () => {
-  return 3;
+const fs = require("fs");
+const numbers = fs
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .split("\n")
+  .map((str) => parseInt(str));
+
+const firstNumNotSumOfTwoPreceedingNNumbers = (numbers, n) => {
+  for (let i = n; i < numbers.length; i++) {
+    const preceedingN = numbers.slice(i - n, i);
+    if (!sumsOfTwo(preceedingN).includes(numbers[i])) {
+      return numbers[i];
+    }
+  }
 };
+
+const sumsOfTwo = (numbers) => {
+  const sums = new Set();
+  numbers.forEach((num1, index1) => {
+    numbers.forEach((num2, index2) => {
+      if (index1 !== index2) sums.add(num1 + num2);
+    });
+  });
+  return Array.from(sums);
+};
+
+console.log(numbers);
+console.log(firstNumNotSumOfTwoPreceedingNNumbers(numbers, 25));
 
 module.exports = {
   firstNumNotSumOfTwoPreceedingNNumbers: firstNumNotSumOfTwoPreceedingNNumbers,
--- 9/1.test.js
+++ 9/1.test.js
@@ -4,17 +4,102 @@ const { firstNumNotSumOfTwoPreceedingNNumbers } = require("./1");
 describe("changeMe", () => {
   const expectations = [
     {
-      numbers: [1, 2, 3],
+      numbers: [1, 2, 4],
       n: 2,
-      first_number_not_the_sum_of_two_preceeding_n_numbers: 3,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 4,
+    },
+    {
+      numbers: [1, 2, 5],
+      n: 2,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 5,
+    },
+    {
+      numbers: [1, 2, 3, 7],
+      n: 2,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 7,
+    },
+    {
+      numbers: [1, 2, 3, 7, 10],
+      n: 2,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 7,
+    },
+    {
+      numbers: [1, 4, 3, 7, 92],
+      n: 3,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 92,
+    },
+    {
+      numbers: [1, 4, 3, 7, 12],
+      n: 3,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 12,
+    },
+    {
+      numbers: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
+        25,
+        65,
+      ],
+      n: 25,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 65,
+    },
+    {
+      numbers: [
+        35,
+        20,
+        15,
+        25,
+        47,
+        40,
+        62,
+        55,
+        65,
+        95,
+        102,
+        117,
+        150,
+        182,
+        127,
+        219,
+        299,
+        277,
+        309,
+        576,
+      ],
+      n: 5,
+      first_number_not_the_sum_of_two_of_preceeding_n_numbers: 127,
     },
   ];
 
   expectations.forEach((test_case) => {
-    test(`first number not the sum of two preceeding ${test_case.n} numbers is ${test_case.first_number_not_the_sum_of_two_preceeding_n_numbers}`, () => {
+    test(`first number not the sum of two preceeding ${test_case.n} numbers is ${test_case.first_number_not_the_sum_of_two_of_preceeding_n_numbers}`, () => {
       expect(
         firstNumNotSumOfTwoPreceedingNNumbers(test_case.numbers, test_case.n)
-      ).toEqual(test_case.first_number_not_the_sum_of_two_preceeding_n_numbers);
+      ).toEqual(
+        test_case.first_number_not_the_sum_of_two_of_preceeding_n_numbers
+      );
     });
   });
 });
