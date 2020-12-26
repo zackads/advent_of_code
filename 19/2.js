e93a388346c91b85781a76b0804434336efc4885
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-12-26 19:07:15 +0000

    Day 19 part 2

--- 19/2.js
+++ 19/2.js
@@ -1,11 +1,6 @@
-// Exactly the same as part 1
-// Just change the new rules from "8: 42 | 42 8" to "8: 42 | 42 42 | 42 42 42 | 42 42 42 42 | 42 42 42 42 42"
-// and "11: 42 31 | 42 11 31" to " 11: 42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31
-// Courtesy of https://www.reddit.com/r/adventofcode/comments/kh0yiw/2020_day_19_part_2_took_me_two_days_to_figure_out/
-
 const fs = require("fs");
 const [rules, messages] = fs
-  .readFileSync(__dirname + "/input.txt")
+  .readFileSync(__dirname + "/input_part2.txt")
   .toString()
   .split("\n\n")
   .map((strings) => strings.split("\n"));
